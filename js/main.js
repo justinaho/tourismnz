ab058705005ec90589b867f3a5da7150670b6d6c
justinaho <cyjustinaho@gmail.com>
2022-06-14 21:26:37 +1200

    styled each accomodation and started the booking pop up

--- js/main.js
+++ js/main.js
@@ -3,10 +3,31 @@
 // }
 
 function toggleResults(){
-  var places = document.getElementById("container-img-results-js");
+  var places = document.getElementById("content-results-js");
   if (places.style.display === "none") {
     places.style.display = "block";
   } else {
     places.style.display = "none";
+  }
+}
+
+
+function bookHostel(){
+  var hostel = document.getElementById('popUpHostel-js');
+
+  if (hostel.style.display === "none") {
+    hostel.style.display = "block";
+  } else {
+    hostel.style.display = "none";
+  }
+}
+
+
+function cross(){
+  var popUp = document.getElementById('popUpHostel-js');
+  if (popUp.style.display === "block") {
+    popUp.style.display = "none";
+  } else {
+    popUp.style.display = "block";
   }
 }
