7f75a9108b57edc4e18e6b354623d430f6bf3eba
justinaho <cyjustinaho@gmail.com>
2022-06-25 22:24:42 +1200

    Created array for meals, working on showing only selected at review stage

--- js/meals.js
+++ js/meals.js
@@ -1,20 +1,99 @@
 /* eslint-disable no-invalid-this */
 /* eslint-disable max-len */
 // Hostel meals: Lunch, Starter, Main, Dessert
-const hostelMeals1 = ['Beef', 'Salt and pepper squid', 'Angus scotch fillet', 'Tiramisu'];
-const hostelMeals2 = ['Pumpkin ravioli (vg)', 'Garlic bread (vg)', 'Grilled portobello mushroom steak (v)', 'Salted caramel chocolate tart (v)'];
+const hotelMeals1 = ['Beef Lasagna', 'Salt and pepper squid', 'Angus scotch fillet', 'Tiramisu'];
+const hotelMeals2 = ['Pumpkin ravioli (vg)', 'Garlic bread (vg)', 'Grilled portobello mushroom steak (v)', 'Salted caramel chocolate tart (v)'];
 
-// const starter1 = ['Beef Lasagna', 'Cheeseburger', 'Quiche', 'Caesar Salad'];
-// const starter2 = ['Pumpkin ravioli (vg)', 'Buffalo Cauliflower(v)', 'Pesto Gnocchi(vg)', 'Tofu Green Curry'];
+const motelMeals1 = ['Beef Lasagna', 'Cheeseburger', 'Quiche', 'Caesar Salad'];
+const motelMeals2 = ['Pumpkin ravioli (vg)', 'Buffalo Cauliflower(v)', 'Pesto Gnocchi(vg)', 'Tofu Green Curry'];
 
-// const main1 = ['Beef Lasagna', 'Cheeseburger', 'Quiche', 'Caesar Salad'];
-// const main2 = ['Pumpkin ravioli (vg)', 'Buffalo Cauliflower(v)', 'Pesto Gnocchi(vg)', 'Tofu Green Curry'];
+const houseMeals1 = ['Beef Lasagna', 'Cheeseburger', 'Quiche', 'Caesar Salad'];
+const houseMeals2 = ['Pumpkin ravioli (vg)', 'Buffalo Cauliflower(v)', 'Pesto Gnocchi(vg)', 'Tofu Green Curry'];
 
-// const dessert1 = ['Beef Lasagna', 'Cheeseburger', 'Quiche', 'Caesar Salad'];
-// const dessert2 = ['Pumpkin ravioli (vg)', 'Buffalo Cauliflower(v)', 'Pesto Gnocchi(vg)', 'Tofu Green Curry'];
+const hotelLunch1 = hotelMeals1[0];
+const hotelLunch2 = hotelMeals2[0];
+const hotelStarter1 = hotelMeals1[1];
+const hotelStarter2 = hotelMeals2[1];
+const hotelMain1 = hotelMeals1[2];
+const hotelMain2 = hotelMeals2[2];
+const hotelDessert1 = hotelMeals1[3];
+const hotelDessert2 = hotelMeals2[3];
 
-const hostelLunch1 = hostelMeals1[0];
-const hostelLunch2 = hostelMeals2[0];
+const motelLunch1 = motelMeals1[0];
+const motelLunch2 = motelMeals2[0];
+const motelStarter1 = motelMeals1[1];
+const motelStarter2 = motelMeals2[1];
+const motelMain1 = motelMeals1[2];
+const motelMain2 = motelMeals2[2];
+const motelDessert1 = motelMeals1[3];
+const motelDessert2 = motelMeals2[3];
+
+const houseLunch1 = houseMeals1[0];
+const houseLunch2 = houseMeals2[0];
+const houseStarter1 = houseMeals1[1];
+const houseStarter2 = houseMeals2[1];
+const houseMain1 = houseMeals1[2];
+const houseMain2 = houseMeals2[2];
+const houseDessert1 = houseMeals1[3];
+const houseDessert2 = houseMeals2[3];
+
+
+const price = [30, 157, 90, 240];
+const hostelPrice = price[0];
+const hotelPrice = price[1];
+const motelPrice = price[2];
+const housePrice = price[3];
+
+
+const buttonList = document.querySelectorAll('.type');
+buttonList.forEach(function(i) {
+  i.addEventListener('click', function(e) {
+    const type = e.target.value;
+    document.getElementById('acommodationType').innerHTML = 'Acommodation type: ' + type;
+
+    const roomsAmount = document.getElementById('sidebar__searchform-js').elements['rooms'].value;
+
+    if (type === 'Hostel') {
+      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * hostelPrice;
+    }
+
+    if (type === 'Hotel') {
+      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * hotelPrice;
+      document.getElementById('lunch1').innerHTML = hotelLunch1;
+      document.getElementById('lunch2').innerHTML = hotelLunch2;
+      document.getElementById('starter1').innerHTML = hotelStarter1;
+      document.getElementById('starter2').innerHTML = hotelStarter2;
+      document.getElementById('main1').innerHTML = hotelMain1;
+      document.getElementById('main2').innerHTML = hotelMain2;
+      document.getElementById('dessert1').innerHTML = hotelDessert1;
+      document.getElementById('dessert2').innerHTML = hotelDessert2;
+    }
+
+    if (type === 'Motel') {
+      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * motelPrice;
+      document.getElementById('lunch1').innerHTML = motelLunch1;
+      document.getElementById('lunch2').innerHTML = motelLunch2;
+      document.getElementById('starter1').innerHTML = motelStarter1;
+      document.getElementById('starter2').innerHTML = motelStarter2;
+      document.getElementById('main1').innerHTML = motelMain1;
+      document.getElementById('main2').innerHTML = motelMain2;
+      document.getElementById('dessert1').innerHTML = motelDessert1;
+      document.getElementById('dessert2').innerHTML = motelDessert2;
+    }
+
+    if (type === 'House') {
+      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * housePrice;
+      document.getElementById('lunch1').innerHTML = houseLunch1;
+      document.getElementById('lunch2').innerHTML = houseLunch2;
+      document.getElementById('starter1').innerHTML = houseStarter1;
+      document.getElementById('starter2').innerHTML = houseStarter2;
+      document.getElementById('main1').innerHTML = houseMain1;
+      document.getElementById('main2').innerHTML = houseMain2;
+      document.getElementById('dessert1').innerHTML = houseDessert1;
+      document.getElementById('dessert2').innerHTML = houseDessert2;
+    }
+  });
+});
 
 $(document).ready(function() {
   $('#display').click(function() {
@@ -27,16 +106,15 @@ $(document).ready(function() {
     });
 
 
-    const lunchOptions = [];
-    $.each($('input[name=\'lunch\']:checked'), function() {
-      lunchOptions.push($(this).val());
-
+    const lunchBox = [hotelLunch1, hotelLunch2, motelLunch1, motelLunch2, houseLunch1, houseLunch2];
+    $.each($('lunchBox:checked'), function() {
+      lunchBox.push($(this).val());
       if ($(this).is(':checked')) {
-        document.getElementById('lunchResult').innerHTML = 'Lunch: ' + lunchOptions.join(', ');
+        document.getElementById('lunchResult').innerHTML = 'Lunch: ' + lunchBox;
       }
     });
 
-    const starterOptions = [];
+    const starterOptions = [hotelStarter1, hotelStarter2, motelStarter1, motelStarter2, houseStarter1, houseStarter2];
     $.each($('input[name=\'starter\']:checked'), function() {
       starterOptions.push($(this).val());
 
@@ -63,12 +141,12 @@ $(document).ready(function() {
     });
 
 
-    let dietaryResult = document.getElementById('dietaryRequirementsResult').innerHTML;
+    // const dietaryResult = document.getElementById('dietaryRequirementsResult').innerHTML;
     const dietaryRequirementsOptions = [];
     $.each($('input[name=\'dietaryRequirements\']:checked'), function() {
       dietaryRequirementsOptions.push($(this).val());
       if ($(this).is(':checked')) {
-        dietaryResult = 'Dietary requirements: ' + dietaryRequirementsOptions.join(', ');
+        document.getElementById('dietaryRequirementsResult').innerHTML = 'Dietary requirements: ' + dietaryRequirementsOptions.join(', ');
       }
     });
 
@@ -91,40 +169,6 @@ $(document).ready(function() {
     const otherOption = document.getElementById('otherOption').value;
     if (document.getElementById('otherOption').value !== '') {
       document.getElementById('dietaryResult').innerHTML = otherOption;
-    }
-  });
-});
-
-const price = [30, 157, 90, 240];
-const hostelPrice = price[0];
-const hotelPrice = price[1];
-const motelPrice = price[2];
-const housePrice = price[3];
-
-
-const buttonList = document.querySelectorAll('.type');
-buttonList.forEach(function(i) {
-  i.addEventListener('click', function(e) {
-    const type = e.target.value;
-    document.getElementById('acommodationType').innerHTML = 'Acommodation type: ' + type;
-
-    const roomsAmount = document.getElementById('sidebar__searchform-js').elements['rooms'].value;
-
-    if (type === 'Hostel') {
-      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * hostelPrice;
-      document.getElementById('lunchBox').innerHTML = hostelLunch1;
-    }
-
-    if (type === 'Hotel') {
-      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * hotelPrice;
-    }
-
-    if (type === 'Motel') {
-      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * motelPrice;
-    }
-
-    if (type === 'House') {
-      document.getElementById('bookingCosts').innerHTML = 'Total cost: $' + roomsAmount * housePrice;
     }
   });
 });
